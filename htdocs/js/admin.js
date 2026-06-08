cecaf4f95463d8d968e7e383626c78c2da52c168
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-06-07 18:42:02 -0700

    chore: fix spacing in JS files

--- htdocs/js/admin.js
+++ htdocs/js/admin.js
@@ -1,156 +1,153 @@
 // admin.js
 // for admin.html
 
-import {
-	NTYPE, NREPS, type, reps
-} from "./pacmacro.js";
+import { NTYPE, NREPS, type, reps } from "./pacmacro.js";
 
 window.onload = () => {
-	let stat = document.getElementById("status");
-	let load = document.getElementById("load");
-	let list = document.getElementById("list");
+  let stat = document.getElementById("status");
+  let load = document.getElementById("load");
+  let list = document.getElementById("list");
 
-	stat.innerHTML = "--"; // no status
+  stat.innerHTML = "--"; // no status
 
-	document.getElementById("all_edible").onclick = async () => {
-		let players;
-		try {
-			players = await fetch("/api/player/list.json");
-			if (!players.ok) {
-				throw "Response not OK.";
-			}
+  document.getElementById("all_edible").onclick = async () => {
+    let players;
+    try {
+      players = await fetch("/api/player/list.json");
+      if (!players.ok) {
+        throw "Response not OK.";
+      }
 
-			players = await players.json();
-		} catch {
-			list.innerHTML = `
+      players = await players.json();
+    } catch {
+      list.innerHTML = `
 				<p>There was a problem contacting the API.</p>
 			`;
-			return;
-		}
+      return;
+    }
 
-		let id   = document.getElementById("id");
-		let pass = document.getElementById("pass");
+    let id = document.getElementById("id");
+    let pass = document.getElementById("pass");
 
-		for (const p of players) {
-			if (p.reps != 3 || p.type == 2)
-				continue
+    for (const p of players) {
+      if (p.reps != 3 || p.type == 2) continue;
 
-			let form = new FormData();
+      let form = new FormData();
 
-			form.append("id", id.value);
-			form.append("pass", pass.value);
-			form.append("type", p.type);
-			form.append("reps", 4); // 4 is RepsEdible
+      form.append("id", id.value);
+      form.append("pass", pass.value);
+      form.append("type", p.type);
+      form.append("reps", 4); // 4 is RepsEdible
 
-			try {
-				let resp = await fetch(`/api/admin/update/${p.id}`, {
-					method: "POST",
-					body: form
-				});
-				if (resp.ok) {
-					stat.innerHTML = `Success ${p.id}`;
-				}
-			} catch {}
-		}
-		list.innerHTML = "Please load players again.";
-	};
+      try {
+        let resp = await fetch(`/api/admin/update/${p.id}`, {
+          method: "POST",
+          body: form,
+        });
+        if (resp.ok) {
+          stat.innerHTML = `Success ${p.id}`;
+        }
+      } catch {}
+    }
+    list.innerHTML = "Please load players again.";
+  };
 
-	document.getElementById("all_nothing").onclick = async () => {
-		let players;
-		try {
-			players = await fetch("/api/player/list.json");
-			if (!players.ok) {
-				throw "Response not OK.";
-			}
+  document.getElementById("all_nothing").onclick = async () => {
+    let players;
+    try {
+      players = await fetch("/api/player/list.json");
+      if (!players.ok) {
+        throw "Response not OK.";
+      }
 
-			players = await players.json();
-		} catch {
-			list.innerHTML = `
+      players = await players.json();
+    } catch {
+      list.innerHTML = `
 				<p>There was a problem contacting the API.</p>
 			`;
-			return;
-		}
+      return;
+    }
 
-		let id   = document.getElementById("id");
-		let pass = document.getElementById("pass");
+    let id = document.getElementById("id");
+    let pass = document.getElementById("pass");
 
-		for (const p of players) {
-			// don't change admin
-			if (p.type == 2)
-				continue
+    for (const p of players) {
+      // don't change admin
+      if (p.type == 2) continue;
 
-			let form = new FormData();
+      let form = new FormData();
 
-			form.append("id", id.value);
-			form.append("pass", pass.value);
-			form.append("type", p.type);
-			form.append("reps", 0); // 0 is RepsNothing
+      form.append("id", id.value);
+      form.append("pass", pass.value);
+      form.append("type", p.type);
+      form.append("reps", 0); // 0 is RepsNothing
 
-			try {
-				let resp = await fetch(`/api/admin/update/${p.id}`, {
-					method: "POST",
-					body: form
-				});
-				if (resp.ok) {
-					stat.innerHTML = `Success ${p.id}`;
-				}
-			} catch {}
-		}
-		list.innerHTML = "Please load players again.";
-	};
+      try {
+        let resp = await fetch(`/api/admin/update/${p.id}`, {
+          method: "POST",
+          body: form,
+        });
+        if (resp.ok) {
+          stat.innerHTML = `Success ${p.id}`;
+        }
+      } catch {}
+    }
+    list.innerHTML = "Please load players again.";
+  };
 
-	load.onclick = async () => {
-		list.innerHTML = ""; // clear list
+  load.onclick = async () => {
+    list.innerHTML = ""; // clear list
 
-		let players;
-		try {
-			players = await fetch("/api/player/list.json");
-			if (!players.ok) {
-				throw "Response not OK.";
-			}
+    let players;
+    try {
+      players = await fetch("/api/player/list.json");
+      if (!players.ok) {
+        throw "Response not OK.";
+      }
 
-			players = await players.json();
-		} catch {
-			list.innerHTML = `
+      players = await players.json();
+    } catch {
+      list.innerHTML = `
 				<p>There was a problem contacting the API.</p>
 			`;
-			return;
-		}
-		// players is a list of all online players
+      return;
+    }
+    // players is a list of all online players
 
-		for (let i = 0; i < players.length; i++) {
-			const p = players[i];
+    for (let i = 0; i < players.length; i++) {
+      const p = players[i];
 
-			let types = "";
-			for (let j = 0; j < NTYPE; j++) {
-				types += `<option value=${j}
+      let types = "";
+      for (let j = 0; j < NTYPE; j++) {
+        types += `<option value=${j}
 					${p.type == j ? "selected" : ""}>
 					${type(j)}
 				</option>`;
-			}
+      }
 
-			let reps_ = "";
-			for (let j = 0; j < NREPS; j++) {
-				reps_ += `<option value=${j}
+      let reps_ = "";
+      for (let j = 0; j < NREPS; j++) {
+        reps_ += `<option value=${j}
 					${p.reps == j ? "selected" : ""}>
 					${reps(j)}
 				</option>`;
-			}
+      }
 
-			let player = document.createElement("div");
-			player.classList.add("player");
+      let player = document.createElement("div");
+      player.classList.add("player");
 
-			if (p.type == 3) // TypeHidden
-				player.classList.add("hidden");
+      if (p.type == 3)
+        // TypeHidden
+        player.classList.add("hidden");
 
-			player.innerHTML = `
+      player.innerHTML = `
 				<h1>${p.name} (${p.id})</h1>
 				<select id="type${i}">${types}</select>
 				<select id="reps${i}">${reps_}</select>
 			`;
 
-			let submit = document.createElement("button");
-			submit.onclick = eval(`
+      let submit = document.createElement("button");
+      submit.onclick = eval(`
 				async () => {
 					let id    = document.getElementById("id");
 					let pass  = document.getElementById("pass");
@@ -183,10 +180,10 @@ window.onload = () => {
 					}
 				}
 			`);
-			submit.innerHTML = `Update ${p.id}`;
+      submit.innerHTML = `Update ${p.id}`;
 
-			player.appendChild(submit);
-			list.appendChild(player);
-		}
-	};
-}
+      player.appendChild(submit);
+      list.appendChild(player);
+    }
+  };
+};
--- htdocs/js/index.js
+++ htdocs/js/index.js
@@ -2,192 +2,206 @@
 // programming for the lobby and PacMacro game
 
 import {
-	ribbons,
-	EXPAND_X, EXPAND_Y,
-	pacmacro_init,
-	connectWS,
-	watchLocation,
-	convertCoords,
-	NREPS,
-	getCredentials,
-	reps,
+  ribbons,
+  EXPAND_X,
+  EXPAND_Y,
+  pacmacro_init,
+  connectWS,
+  watchLocation,
+  convertCoords,
+  NREPS,
+  getCredentials,
+  reps,
 } from "./pacmacro.js";
 
 window.onload = async () => {
-	ribbons();
-	// reset globals
-	pacmacro_init();
+  ribbons();
+  // reset globals
+  pacmacro_init();
 
-	let self_summary = document.getElementById("self-summary");
-	let pacmacro_status = document.getElementById("pacmacro-status");
-	let pacmacro_canvas = document.getElementById("pacmacro-canvas");
-	let pass = ""; // received upon window.prompt(...)
+  let self_summary = document.getElementById("self-summary");
+  let pacmacro_status = document.getElementById("pacmacro-status");
+  let pacmacro_canvas = document.getElementById("pacmacro-canvas");
+  let pass = ""; // received upon window.prompt(...)
 
-	pacmacro_status.style.visibility = "hidden"; // hides debug
+  pacmacro_status.style.visibility = "hidden"; // hides debug
 
-	try {
-		let pacmacro_map = await fetch("/api/game/map.json");
-		window.pacmacro_map = await pacmacro_map.json();
-	} catch {
-		pacmacro_status.innerHTML = "Error: Couldn't receive map information.";
-		window.pacmacro_ws.close(); // close websocket connection
-		return; // do not proceed
-	}
+  try {
+    let pacmacro_map = await fetch("/api/game/map.json");
+    window.pacmacro_map = await pacmacro_map.json();
+  } catch {
+    pacmacro_status.innerHTML = "Error: Couldn't receive map information.";
+    if (window.pacmacro_map) {
+      window.pacmacro_ws.close(); // close websocket connection
+    }
+    return; // do not proceed
+  }
 
-	window.pacmacro_ctx = pacmacro_canvas.getContext("2d");
-	// set canvas size as per window.pacmacro_map
-	window.pacmacro_ctx.canvas.width = window.pacmacro_map.width * EXPAND_X;
-	window.pacmacro_ctx.canvas.height = window.pacmacro_map.height * EXPAND_Y;
-	window.pacmacro_ctx.font = "16pt sans-serif";
+  window.pacmacro_ctx = pacmacro_canvas.getContext("2d");
+  // set canvas size as per window.pacmacro_map
+  window.pacmacro_ctx.canvas.width = window.pacmacro_map.width * EXPAND_X;
+  window.pacmacro_ctx.canvas.height = window.pacmacro_map.height * EXPAND_Y;
+  window.pacmacro_ctx.font = "16pt sans-serif";
 
-	let pacmacro_draw = () => {
-		console.log("draw");
+  let pacmacro_draw = () => {
+    console.log("draw");
 
-		if (window.pacmacro_ctx === undefined)
-			return;
+    if (window.pacmacro_ctx === undefined) return;
 
-		// fill canvas with map drawing
-		window.pacmacro_ctx.drawImage(
-		        window.pacmacro_img_map, 0, 0,
-		        window.pacmacro_ctx.canvas.width,
-		        window.pacmacro_ctx.canvas.height);
+    // fill canvas with map drawing
+    window.pacmacro_ctx.drawImage(
+      window.pacmacro_img_map,
+      0,
+      0,
+      window.pacmacro_ctx.canvas.width,
+      window.pacmacro_ctx.canvas.height,
+    );
 
-		// render each player
-		Object.keys(window.pacmacro_players).forEach((ID,i) => {
-			console.log("---- DRAWING ----");
+    // render each player
+    Object.keys(window.pacmacro_players).forEach((ID, i) => {
+      console.log("---- DRAWING ----");
 
-			const p = window.pacmacro_players[ID];
-			if (p.player.type == 3) // TypeHidden
-				return;
-			let img, text;
+      const p = window.pacmacro_players[ID];
+      if (p.player.type == 3)
+        // TypeHidden
+        return;
+      let img, text;
 
-			console.log(p);
+      console.log(p);
 
-			if (p.player.type == 1) {
-				if (p.player.reps == 1) {
-					img = window.pacmacro_img_coin;
-				} else {
-					img = window.pacmacro_img_leader;
-				}
-			} else {
-				switch (p.player.reps) {
-				case 1: // pacman
-					img = window.pacmacro_img_pacman;
-					break;
-				case 2: // antipac
-					img = window.pacmacro_img_anti;
-					break;
-				case 3: // ghost
-					img = window.pacmacro_img_ghost;
-					break;
-				case 4: // edible
-					img = window.pacmacro_img_edible;
-					break;
-				default: // nothing; watcher; error
-					return;
-				}
-			}
+      if (p.player.type == 1) {
+        if (p.player.reps == 1) {
+          img = window.pacmacro_img_coin;
+        } else {
+          img = window.pacmacro_img_leader;
+        }
+      } else {
+        switch (p.player.reps) {
+          case 1: // pacman
+            img = window.pacmacro_img_pacman;
+            break;
+          case 2: // antipac
+            img = window.pacmacro_img_anti;
+            break;
+          case 3: // ghost
+            img = window.pacmacro_img_ghost;
+            break;
+          case 4: // edible
+            img = window.pacmacro_img_edible;
+            break;
+          default: // nothing; watcher; error
+            return;
+        }
+      }
 
-			window.pacmacro_ctx.drawImage(img,
-				p.plot.x * EXPAND_X - 48,
-				p.plot.y * EXPAND_Y - 88,
-				96, 96);
-			window.pacmacro_ctx.textAlign = "center";
-			window.pacmacro_ctx.fillStyle = "#ffffff";
+      window.pacmacro_ctx.drawImage(
+        img,
+        p.plot.x * EXPAND_X - 48,
+        p.plot.y * EXPAND_Y - 88,
+        96,
+        96,
+      );
+      window.pacmacro_ctx.textAlign = "center";
+      window.pacmacro_ctx.fillStyle = "#ffffff";
 
-			if (ID == window.pacmacro_ID) {
-				text = `${p.player.name} (You)`;
-			} else {
-				text = `${p.player.name} (${ID})`;
-			}
+      if (ID == window.pacmacro_ID) {
+        text = `${p.player.name} (You)`;
+      } else {
+        text = `${p.player.name} (${ID})`;
+      }
 
-			console.log(p.plot);
+      console.log(p.plot);
 
-			window.pacmacro_ctx.fillText(text,
-				p.plot.x * EXPAND_X,
-				p.plot.y * EXPAND_Y - 112);
-		});
-	};
+      window.pacmacro_ctx.fillText(
+        text,
+        p.plot.x * EXPAND_X,
+        p.plot.y * EXPAND_Y - 112,
+      );
+    });
+  };
 
-	let pacmacro_open = async () => {
-		// send password to authenticate player
-		window.pacmacro_ws.send(pass);
+  let pacmacro_open = async () => {
+    // send password to authenticate player
+    window.pacmacro_ws.send(pass);
 
-		watchLocation((p) => {
-			let coordinate = {
-				"latitude": p.coords.latitude,
-				"longitude": p.coords.longitude
-			};
+    watchLocation((p) => {
+      let coordinate = {
+        latitude: p.coords.latitude,
+        longitude: p.coords.longitude,
+      };
 
-			window.pacmacro_ws.send(JSON.stringify(coordinate));
-		});
+      window.pacmacro_ws.send(JSON.stringify(coordinate));
+    });
 
-		pacmacro_status.innerHTML = "Good to go.";
-	};
+    pacmacro_status.innerHTML = "Good to go.";
+  };
 
-	let pacmacro_redirect = () => {
-		// window.location.href = "/login";
-	};
+  let pacmacro_redirect = () => {
+    // window.location.href = "/login";
+  };
 
-	let pacmacro_recv = (e) => {
-		let msg = JSON.parse(e.data);
-		console.log(`COMMAND: ${msg.command}`);
-		console.log(`DATA: ${msg.data}`);
+  let pacmacro_recv = (e) => {
+    let msg = JSON.parse(e.data);
+    console.log(`COMMAND: ${msg.command}`);
+    console.log(`DATA: ${msg.data}`);
 
-		if (msg.command === undefined)
-			return; // invalid message
+    if (msg.command === undefined) return; // invalid message
 
-		if (msg.command == "inform") {
-			let plot = convertCoords(window.pacmacro_map,
-				msg.coordinate.latitude, msg.coordinate.longitude);
-			const p = JSON.parse(msg.data);
+    if (msg.command == "inform") {
+      let plot = convertCoords(
+        window.pacmacro_map,
+        msg.coordinate.latitude,
+        msg.coordinate.longitude,
+      );
+      const p = JSON.parse(msg.data);
 
-			if (p.id === undefined)
-				return; // invalid
+      if (p.id === undefined) return; // invalid
 
-			// set player
-			window.pacmacro_players[p.id] = {
-				"plot": plot,
-				"player": p
-			};
+      // set player
+      window.pacmacro_players[p.id] = {
+        plot: plot,
+        player: p,
+      };
 
-			if (p.id == window.pacmacro_ID) {
-				self_summary.innerHTML =
-					`${p.name} (${p.id}) is ${reps(p.reps)}`;
-			}
-		} else
-		if (msg.command == "move") {
-			console.log("moving");
-			const p = window.pacmacro_players[msg.data]; // data is ID
+      if (p.id == window.pacmacro_ID) {
+        self_summary.innerHTML = `${p.name} (${p.id}) is ${reps(p.reps)}`;
+      }
+    } else if (msg.command == "move") {
+      console.log("moving");
+      const p = window.pacmacro_players[msg.data]; // data is ID
 
-			if (p === undefined)
-				return; // invalid
+      if (p === undefined) return; // invalid
 
-			// update plot
-			p.plot = convertCoords(window.pacmacro_map,
-				msg.coordinate.latitude, msg.coordinate.longitude);
-		}
+      // update plot
+      p.plot = convertCoords(
+        window.pacmacro_map,
+        msg.coordinate.latitude,
+        msg.coordinate.longitude,
+      );
+    }
 
-		// write server message to status element
-		pacmacro_status.innerHTML = e.data;
-		pacmacro_draw(); // re-draw the map; maybe there was a player update
-	};
+    // write server message to status element
+    pacmacro_status.innerHTML = e.data;
+    pacmacro_draw(); // re-draw the map; maybe there was a player update
+  };
 
-	const params = new URLSearchParams(window.location.search);
+  const params = new URLSearchParams(window.location.search);
 
-	const { ID, password } = getCredentials();
+  const { ID, password } = getCredentials();
 
-	if (ID.length == 0 || password.length == 0) {
-		window.location.href = '/register';
-		return;
-	}
+  if (ID.length == 0 || password.length == 0) {
+    window.location.href = "/register";
+    return;
+  }
 
-	pass = password;
-	window.pacmacro_ID = ID;
+  pass = password;
+  window.pacmacro_ID = ID;
 
-	connectWS(ID,
-		pacmacro_open, // on open
-		pacmacro_redirect, // on close
-		pacmacro_redirect, // on error
-		pacmacro_recv); // on message
-}
+  connectWS(
+    ID,
+    pacmacro_open, // on open
+    pacmacro_redirect, // on close
+    pacmacro_redirect, // on error
+    pacmacro_recv,
+  ); // on message
+};
